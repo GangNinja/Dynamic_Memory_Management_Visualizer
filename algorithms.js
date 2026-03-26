9876bce46201d41259bbb0a0e1ab5ee880496c9f
Diwakar Kaushik <devkaushik6906@gmail.com>
2026-03-27 00:01:36 +0530

    Refactor memory allocation functions and add paging algorithms

--- algorithms.js
+++ algorithms.js
@@ -78,3 +78,121 @@ return true
 
 return false
 }
+
+// Paging Algorithms - return {faults, frames, trace}
+function fifoPaging() {
+  let queue = []
+  let localFrames = [...frames]
+  let localFaults = 0
+  let localTrace = []
+  
+  for(let i = 0; i < refs.length; i++) {
+    let page = refs[i]
+    let fault = true
+    let frameIdx = localFrames.indexOf(page)
+    
+    if (frameIdx !== -1) {
+      fault = false
+    } else if (localFrames.some(f => f === -1)) {
+      let emptyIdx = localFrames.indexOf(-1)
+      localFrames[emptyIdx] = page
+    } else {
+      let victim = queue.shift()
+      let victimIdx = localFrames.indexOf(victim)
+      localFrames[victimIdx] = page
+    }
+    
+    queue.push(page)
+    if (fault) localFaults++
+    localTrace.push({
+      ref: page,
+      frames: [...localFrames],
+      fault: fault ? 'FAULT' : 'HIT'
+    })
+  }
+  
+  return {faults: localFaults, frames: localFrames, trace: localTrace}
+}
+
+function lruPaging() {
+  let localFrames = [...frames]
+  let localFaults = 0
+  let localTrace = []
+  let useOrder = []
+  
+  for(let i = 0; i < refs.length; i++) {
+    let page = refs[i]
+    let frameIdx = localFrames.indexOf(page)
+    let fault = frameIdx === -1
+    
+    if (fault) {
+      if (localFrames.some(f => f === -1)) {
+        let emptyIdx = localFrames.indexOf(-1)
+        localFrames[emptyIdx] = page
+        useOrder.push(page)
+      } else {
+        // Find LRU
+        let lruPage = useOrder[0]
+        let lruIdx = localFrames.indexOf(lruPage)
+        localFrames[lruIdx] = page
+        useOrder.shift()
+        useOrder.push(page)
+      }
+      localFaults++
+    } else {
+      // Update use order
+      useOrder = useOrder.filter(p => p !== page)
+      useOrder.push(page)
+    }
+    
+    localTrace.push({
+      ref: page,
+      frames: [...localFrames],
+      fault: fault ? 'FAULT' : 'HIT'
+    })
+  }
+  
+  return {faults: localFaults, frames: localFrames, trace: localTrace}
+}
+
+function optimalPaging() {
+  let localFrames = [...frames]
+  let localFaults = 0
+  let localTrace = []
+  
+  for(let i = 0; i < refs.length; i++) {
+    let page = refs[i]
+    let frameIdx = localFrames.indexOf(page)
+    let fault = frameIdx === -1
+    
+    if (fault) {
+      if (localFrames.some(f => f === -1)) {
+        let emptyIdx = localFrames.indexOf(-1)
+        localFrames[emptyIdx] = page
+      } else {
+        // Find page with farthest next use
+        let farthestIdx = -1
+        let maxDist = -1
+        for(let j = 0; j < localFrames.length; j++) {
+          let p = localFrames[j]
+          let nextUse = refs.slice(i+1).indexOf(p)
+          if (nextUse === -1) nextUse = Infinity
+          if (nextUse > maxDist) {
+            maxDist = nextUse
+            farthestIdx = j
+          }
+        }
+        localFrames[farthestIdx] = page
+      }
+      localFaults++
+    }
+    
+    localTrace.push({
+      ref: page,
+      frames: [...localFrames],
+      fault: fault ? 'FAULT' : 'HIT'
+    })
+  }
+  
+  return {faults: localFaults, frames: localFrames, trace: localTrace}
+}
